11c7d0ad11640a7e012c66ecb93b8178cc8aa23d
dvejmz <dvejmz@gmail.com>
2020-01-18 20:14:32 +0000

    Refactor App to use React reducers for state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import combineReducers from 'react-combine-reducers';
 import './App.scss';
 import MakePaperButton from './components/MakePaperButton';
 import PurchasableResource from './components/PurchasableResource';
@@ -29,58 +30,89 @@ interface AppProps {
   initialState: GameState;
 }
 
+const RESOURCES_BUY_PULP = 'RESOURCES_BUY_PULP';
+const RESOURCES_MAKE_PAPER = 'RESOURCES_MAKE_PAPER';
+const RESOURCES_SELL_PAPER = 'RESOURCES_SELL_PAPER';
+
+const fundsReducer = (currentFunds: number, action: { type: string, data?: any }) => {
+  switch (action.type) {
+    case RESOURCES_BUY_PULP:
+      return currentFunds - action.data.price;
+    case RESOURCES_SELL_PAPER:
+      return currentFunds + action.data.price;
+    default:
+      return currentFunds;
+  }
+};
+
+const getUpdatedPaperCounter = (resources: PlayerResources): number => (
+  resources.pulp
+    ? resources.paper + 1
+    : resources.paper
+);
+
+const getUpdatedPulpCounter = (resources: PlayerResources): number => (
+  Math.max(resources.pulp - 1, 0)
+);
+
+const resourcesReducer = (currentResources: PlayerResources, action: { type: string, data?: any }): PlayerResources => {
+  switch (action.type) {
+    case RESOURCES_BUY_PULP: {
+      return { ...currentResources, pulp: currentResources.pulp + action.data.purchaseRate };
+    }
+    case RESOURCES_MAKE_PAPER: {
+      return { ...currentResources, paper: getUpdatedPaperCounter(currentResources), pulp: getUpdatedPulpCounter(currentResources) };
+    }
+    case RESOURCES_SELL_PAPER: {
+      return { ...currentResources, paper: currentResources.paper ? currentResources.paper - 1 : currentResources.paper };
+    }
+    default:
+      return currentResources;
+  }
+};
+
+const resourcePricesReducer = (resourcePrices: any, action: any) => {
+  return resourcePrices;
+};
+
+const purchaseRatesReducer = (purchaseRates: any, action: any) => {
+  return purchaseRates;
+};
+
 const App: React.FC<AppProps> = (props: AppProps) => {
   const { initialState } = props;
-  const [ resources, setResources ] = useState(initialState.resources);
-  const [ funds, setFunds ] = useState(initialState.funds);
-  const [ purchaseRates, setPurchaseRates ] = useState(initialState.purchaseRates);
-  const [ resourcePrices, setResourcePrices ] = useState(initialState.resourcePrices);
+  const [ rootReducer, rootState ] = combineReducers({
+    funds: [ fundsReducer, initialState.funds ],
+    resources: [ resourcesReducer, initialState.resources ],
+    resourcePrices: [ resourcePricesReducer, initialState.resourcePrices ],
+    purchaseRates: [ purchaseRatesReducer, initialState.purchaseRates ],
+  });
+  const [ state, dispatch ]: any[] = useReducer(rootReducer, rootState);
+  // TODO -- Collapse purchase rates and resource prices into resources object
+  // if React re-renders allow it
+  const { resources, funds, purchaseRates, resourcePrices } = state;
 
-  const getUpdatedPaperCounter = (): number => (
-    resources.pulp
-      ? resources.paper + 1
-      : resources.paper
-  );
-
-  const getUpdatedPulpCounter = (): number => (
-    Math.max(resources.pulp - 1, 0)
-  );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (resources.paper) {
+        dispatch({
+          type: RESOURCES_SELL_PAPER,
+          data: { price: 1 },
+        });
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
 
   const handleMakePaperButtonClick = () => {
-    setResources({
-      ...resources,
-      paper: getUpdatedPaperCounter(),
-      pulp: getUpdatedPulpCounter(),
-    });
+    dispatch({ type: RESOURCES_MAKE_PAPER });
   };
-
-  const increasePulpByPurchaseRate = (): number => (
-    resources.pulp + purchaseRates.pulp
-  );
-
-  const subtractPulpPriceFromFunds = (): number => (
-    Math.max(funds - resourcePrices.pulp, 0)
-  );
 
   const handleBuyPulpClick = () => {
     if (!funds) {
       return;
     }
-    setResources({
-      ...resources,
-      pulp: increasePulpByPurchaseRate(),
-    });
-    setFunds(subtractPulpPriceFromFunds());
-  };
-
-  const doPaperSaleCycle = () => {
-    if (resources.paper) {
-      setFunds(funds + 3);
-      setResources({
-        ...resources,
-        paper: resources.paper - 1,
-      });
-    }
+    dispatch({ type: RESOURCES_BUY_PULP, data: { purchaseRate: purchaseRates.pulp, price: resourcePrices.pulp }});
   };
 
   return (
