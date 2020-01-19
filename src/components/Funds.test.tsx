93a2cf01c739a1c9091e0fc861b8e65772c14e87
dvejmz <dvejmz@gmail.com>
2020-01-19 02:45:12 +0000

    Show funds to two decimal places

--- src/components/Funds.test.tsx
+++ src/components/Funds.test.tsx
@@ -10,10 +10,15 @@ describe('<Funds />', () => {
   });
 
   it('shows amount', () => (
-    expect(funds.find('.resources__funds-amount').text()).toEqual('100')
+    expect(funds.find('.resources__funds-amount').text()).toEqual('100.00')
   ));
 
   it('shows currency', () => (
     expect(funds.find('.resources__funds-currency').text()).toEqual('£')
   ));
+
+  it('shows currency rounded to 2 decimal places', () => {
+    funds.setProps({ amount: 3.5000002324, currency: '£' });
+    expect(funds.find('.resources__funds-amount').text()).toEqual('3.50')
+  })
 });
--- src/components/Funds.tsx
+++ src/components/Funds.tsx
@@ -8,7 +8,7 @@ interface FundsProps {
 const Funds: React.FC<FundsProps> = (props: FundsProps) => {
   return (
     <div className="resources__funds">
-      Funds: <span className="resources__funds-currency">{props.currency}</span><span className="resources__funds-amount">{props.amount}</span>
+      Funds: <span className="resources__funds-currency">{props.currency}</span><span className="resources__funds-amount">{props.amount.toFixed(2)}</span>
     </div>
   );
 };
